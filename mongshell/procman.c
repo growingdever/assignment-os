2cbd16693c54b172f7c440e0167c45574f083acc
loki <growingdever@gmail.com>
2015-09-20 04:25:24 +0900

    흐름 개선을 좀 하긴 했는데 일단 piping 제대로 안 되는 문제가 여전히 남아있음. 안전빵을 위해 커밋함.

--- mongshell/procman.c
+++ mongshell/procman.c
@@ -14,6 +14,7 @@
 
 #define equal_str(str1, str2) strcmp(str1, str2) == 0
 #define memset_zero(var) memset(var, 0, sizeof(var))
+#define dup3(fd1, fd2) do { dup2(fd1, fd2); close(fd1); } while (0);
 
 #define FD_STDIN 0
 #define FD_STDOUT 1
@@ -99,7 +100,7 @@ typedef struct command_line command_line;
 
 void signal_handler(int signo);
 int run(const char*);
-int process_line(const char*);
+command_line* validate_command_line(char*);
 int process_command_line(command_line*);
 command_line* find_command_line_by_id(const char*);
 int test_format(const char*);
@@ -108,7 +109,6 @@ struct command_line* tokenizing_line(const char*);
 char* paxtok (char*, char*);
 char* trim_whitespace(char*);
 char** build_argv(command_line*);
-int piping_if_exist_pipe_id(command_line*);
 
 int line_number;
 struct list* command_line_list;
@@ -173,48 +173,61 @@ int run(const char* filename) {
 		memset_zero(line);
 		fgets(line, sizeof(line), fp);
 		line_number ++;
-		
-		// remove LF
-		if (line[strlen(line) - 1] == '\n') {
-			line[strlen(line) - 1] = 0;
-		}
-		
-		// pass blank line
-		if (strlen(line) == 0) {
+
+		command_line* cmd_line = validate_command_line(line);
+		if (cmd_line == NULL) {
 			continue;
 		}
-		
-		// skip comment line
-		if (line[0] == '#') {
-			continue;
-		}
-		
-		if (process_line(line) < 0) {
-			continue;
-		}
+
+		list_push_back(command_line_list, cmd_line);
+		pipe(cmd_line->pipe_filedes);
 	}
 
-	// little sleep before end program
-	sleep(3);
+	for (int i = 0; i < command_line_list->num_of_elements; i++) {
+		command_line* cmd_line = (command_line*)command_line_list->elements[i];
+		process_command_line(cmd_line);
+	}
+
+	pid_t pid;
+	int status;
+	while ((pid = wait(&status)) != -1)
+		fprintf(stderr, "process %d exits with %d\n", pid, WEXITSTATUS(status));
 	
 	return 0;
 }
 
-int process_line(const char* line) {
+command_line* validate_command_line(char* line) {
+	// remove LF
+	if (line[strlen(line) - 1] == '\n') {
+		line[strlen(line) - 1] = 0;
+	}
+	
+	// pass blank line
+	if (strlen(line) == 0) {
+		return NULL;
+	}
+	
+	// skip comment line
+	if (line[0] == '#') {
+		return NULL;
+	}
+
 	if (test_format(line) < 0) {
-		return -1;
+		return NULL;
 	}
 	
 	struct command_line* cmd_line = tokenizing_line(line);
 
 	if (id_validate(cmd_line->id) < 0) {
 		fprintf(stderr, "invalid id \'%s\' in line %d, ignored\n", cmd_line->id, line_number);
-		return -1;
+		free(cmd_line);
+		return NULL;
 	}
 
 	if (find_command_line_by_id(cmd_line->id) != NULL) {
 		fprintf(stderr, "duplicate id \'%s\' in line %d, ignored\n", cmd_line->id, line_number);
-		return -1;
+		free(cmd_line);
+		return NULL;
 	}
 	
 	if (strlen(cmd_line->action) == 0 ||
@@ -222,7 +235,8 @@ int process_line(const char* line) {
 			&& ! equal_str (cmd_line->action, "wait")
 			&& ! equal_str (cmd_line->action, "respawn"))) {
 		fprintf(stderr, "invalid action \'%s\' in line %d, ignored\n", cmd_line->action, line_number);
-		return -1;
+		free(cmd_line);
+		return NULL;
 	}
 
 	if (strlen(cmd_line->pipe_id) > 0) {
@@ -234,64 +248,67 @@ int process_line(const char* line) {
 				fprintf(stderr, "unknown pipe-id \'%s\' in line %d, ignored\n", cmd_line->pipe_id, line_number);
 			}
 			
-			return -1;
+			free(cmd_line);
+			return NULL;
 		} else {
 			if (equal_str(cmd_line->action, "respawn") || equal_str(target_command_line->action, "respawn")) {
 				fprintf(stderr, "pipe not allowed for \'respawn\' tasks in line %d, ignored\n", line_number);
-				return -1;
+				free(cmd_line);
+				return NULL;
 			}
 
 			if (target_command_line->piped) {
 				fprintf(stderr, "pipe not allowed for already piped tasks in line %d, ignored\n", line_number);
-				return -1;
+				free(cmd_line);
+				return NULL;
 			}
+
+			target_command_line->piped = 1;
+			target_command_line->target_command_line = cmd_line;
+
+			cmd_line->piped = 1;
+			cmd_line->target_command_line = target_command_line;
 		}
 	}
 
 	if (strlen(cmd_line->command) == 0) {
 		fprintf(stderr, "empty command in line %d, ignored\n", line_number);
-		return -1;
+		free(cmd_line);
+		return NULL;
 	}
 
-	list_push_back(command_line_list, cmd_line);
-
-	process_command_line(cmd_line);
-
-	return 0;
+	return cmd_line;
 }
 
 int process_command_line(command_line* cmd_line) {
-	if (piping_if_exist_pipe_id(cmd_line) < 0) {
-		return -1;
-	}
+	int child_pid = fork();
+	if (child_pid == 0) {
+		dup3(cmd_line->pipe_filedes[0], FD_STDIN);
+		dup3(cmd_line->pipe_filedes[1], FD_STDOUT);
 
-	if (fork() == 0) {
+		if (cmd_line->piped) {
+			// dup3(cmd_line->pipe_filedes[0], cmd_line->target_command_line->pipe_filedes[1]);
+			// dup3(cmd_line->pipe_filedes[1], cmd_line->target_command_line->pipe_filedes[0]);
+
+			dup3(cmd_line->target_command_line->pipe_filedes[0], FD_STDIN);
+			dup3(cmd_line->target_command_line->pipe_filedes[1], FD_STDOUT);
+		} else {
+			
+		}
+
 		usleep(DELAY_EXEC_IN_MICRO_SECOND);
 
-		int child_pid = fork();
-		if (child_pid == 0) {
-			if (cmd_line->piped) {
-				dup2(cmd_line->target_command_line->pipe_filedes[0], FD_STDIN);
-				dup2(cmd_line->target_command_line->pipe_filedes[1], FD_STDOUT);
-			} else {
-				dup2(cmd_line->pipe_filedes[0], FD_STDIN);
-				dup2(cmd_line->pipe_filedes[1], FD_STDOUT);
-			}
+		char **argv = build_argv(cmd_line);
+		execvp(argv[0], argv);
+		fprintf(stderr, "failed to execute command \'%s\': No such file or directory\n", argv[0]);
+		exit(-1);
+	}
 
-			char **argv = build_argv(cmd_line);
-			execvp(argv[0], argv);
-			fprintf(stderr, "failed to execute command \'%s\': No such file or directory\n", argv[0]);
-			exit(-1);
-		}
+	cmd_line->pid = child_pid;
 
-		cmd_line->pid = child_pid;
-
-		if (equal_str(cmd_line->action, "wait")) {
-			int status;
-			waitpid(child_pid, &status, 0);
-		}
-
-		exit(0);
+	if (equal_str(cmd_line->action, "wait")) {
+		int status;
+		waitpid(child_pid, &status, 0);
 	}
 
 	return 0;
@@ -456,25 +473,4 @@ char** build_argv(command_line* cmd_line) {
 	argv[i] = NULL;
 
 	return argv;
-}
-
-int piping_if_exist_pipe_id(command_line* cmd_line) {
-	pipe(cmd_line->pipe_filedes);
-
-	dup2(cmd_line->pipe_filedes[0], FD_STDIN);
-	dup2(cmd_line->pipe_filedes[1], FD_STDOUT);
-
-	command_line* target_command_line = NULL;
-	if (strlen(cmd_line->pipe_id) != 0) {
-		target_command_line = find_command_line_by_id(cmd_line->pipe_id);
-		
-		target_command_line->piped = 1;
-		cmd_line->piped = 1;
-		cmd_line->target_command_line = target_command_line;
-
-		dup2(target_command_line->pipe_filedes[0], cmd_line->pipe_filedes[1]);
-		dup2(target_command_line->pipe_filedes[1], cmd_line->pipe_filedes[0]);
-	}
-
-	return 0;
 }
